d56e7cb5ac27b807348aa9d50b15a4eadc25f80e
Emilie <emiliea@udel.edu>
2023-02-15 11:18:30 -0500

    Task3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Button, Container, Col, Row } from "react-bootstrap";
 import "./App.css";
+import lucy from "./lucy.jpg";
 
 function App(): JSX.Element {
     return (
@@ -11,6 +13,37 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. Hello World. This page
                 will automatically reload.
             </p>
+            <h1>This is about my cat!</h1>
+            <img src={lucy} className="App-image" alt="A picture of my cat" />
+            <p>These are the things she likes:</p>
+            <ul className="App-list">
+                <li>Treats</li>
+                <li>Toys</li>
+                <li>Me!</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>First Column!</Col>
+                    <Col>Second Column!</Col>
+                </Row>
+                <Row>
+                    <Col
+                        className="App-firstCol"
+                        style={{ border: "5px solid white", padding: "4px" }}
+                    >
+                        Pretty red rectangle
+                    </Col>
+                    <Col
+                        className="App-secondCol"
+                        style={{ border: "5px solid white", padding: "4px" }}
+                    >
+                        Another pretty red rectangle
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
