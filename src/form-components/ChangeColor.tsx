5a4913d5300b75960c5ee217ee805c57e52bdf72
Emilie <emiliea@udel.edu>
2023-03-19 15:21:05 -0400

    task 10 components pass all tests

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,50 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+// <div data-testid="something" style={{ backgroundColor: a color}}>
 
 export function ChangeColor(): JSX.Element {
+    const [currColor, setCurrColor] = useState<string>("");
+    const colorOptions = [
+        "red",
+        "blue",
+        "green",
+        "orange",
+        "purple",
+        "cyan",
+        "magenta",
+        "white",
+        "black"
+    ];
+    function changeColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setCurrColor(event.target.value);
+        event.target.checked = true;
+    }
     return (
         <div>
             <h3>Change Color</h3>
+            {colorOptions.map((color: string) => (
+                <Form.Check
+                    inline={true}
+                    type="radio"
+                    name="colors"
+                    key={color}
+                    id={color}
+                    label={color}
+                    value={color}
+                    checked={currColor === color}
+                    onChange={changeColor}
+                    style={{ backgroundColor: color }}
+                ></Form.Check>
+            ))}
+            <br></br>
+            You have chosen{" "}
+            <span
+                data-testid="colored-box"
+                style={{ backgroundColor: currColor }}
+            >
+                {currColor}
+            </span>
+            .
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,55 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
 
 export function EditMode(): JSX.Element {
+    const [inEditMode, setInEditMode] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setInEditMode(event.target.checked);
+    }
+    function updateName(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+    function updateStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
+    function printStatus(): string {
+        const studentStatus: string = isStudent
+            ? " is a student."
+            : " is not a student.";
+        return name + studentStatus;
+    }
     return (
-        <div>
-            <h3>Edit Mode</h3>
-        </div>
+        <>
+            <div>
+                <h3>Edit Mode</h3>
+                <Form.Check
+                    type="switch"
+                    id="in-edit-mode-check"
+                    label="Enter edit mode?"
+                    checked={inEditMode}
+                    onChange={updateEditMode}
+                ></Form.Check>
+                {inEditMode ? (
+                    <Form.Group>
+                        <Form.Check
+                            type="checkbox"
+                            id="is-student-check"
+                            label="Are you a student?"
+                            checked={isStudent}
+                            onChange={updateStudent}
+                        ></Form.Check>
+                        <Form.Control
+                            type="string"
+                            value={name}
+                            onChange={updateName}
+                        ></Form.Control>
+                    </Form.Group>
+                ) : (
+                    printStatus()
+                )}
+            </div>
+        </>
     );
 }
